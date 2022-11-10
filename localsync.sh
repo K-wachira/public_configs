91f43d4e14bdaef0429fe312041d62f2342ed39c
k-wachira <abkelvinwachira@gmail.com>
2022-11-10 23:16:32 +0300

    Added dwm configs

---
+++ localsync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR=$HOME/.config/dwm
+CONFIG_DIR=$HOME/.config
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+clear
+
+# Copy dwm for backup
+echo -e "\e[1;31m${bold}Copying dwm to .kelvin-configs ... ${normal}\e[0m"
+cp -r $CONFIG_DIR/dwm/* ./dwm
+echo -e "\e[1;31m${bold}Done copying dwm to .kelvin-configs${normal}\e[0m"
+
+
