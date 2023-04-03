111ad21334557fba3deb2429d6e5797b032cb031
k-wachira <abkelvinwachira@gmail.com>
2023-04-03 22:43:13 +0300

    Install script adaptation for wayland from x11

--- install.sh
+++ install.sh
@@ -10,15 +10,14 @@ clear
 #Dependencies
 echo -e "\e[1;31m${bold}Installing dependencies${normal}\e[0m"
 yay -Syu --noconfirm 'lxappearance' 'rofi' 'dunst' 'alsa-utils' 'feh'  'bluez' 'ntfs-3g' 'arandr' 'gruvbox-dark-gtk'\
-  'betterlockscreen' 'xfce-polkit' 'xfce4-power-manager' 'brightnessctl'\
-  'xorg-xsetroot'  'meson' 'sddm' 'mcfly' 'starship'\
+  'sddm' 'mcfly' 'stow'\
   'zathura' 'zathura-pdf-mupdf' 'ninja' 'lf'\
   'neovim' 'apple-fonts' 'libconfig' 'spotify'\
   'qt5-styleplugins' 'cmake' 'bluez-utils'\
   'rustup' 'python' 'python-pip' 'zsh' \
-  'mpd' 'mpc' 'playerctl' 'pamixer' 'spicetify-cli' \
+  'playerctl' 'pamixer' 'spicetify-cli' \
   'microsoft-edge-dev-bin' 'alacritty' 'visual-studio-code-bin' 'parcellite'\
-  'postman-bin' 'flameshot' 'spotify' --needed
+  'postman-bin' 'flameshot' 'spotify' 'fzf' 'exa' 'zoxide' --needed
 
 echo -e "DONE with yay install"
 sudo pacman -S --noconfirm 'qt5' 'gstreamer' 'gst-libav' 'gst-plugins-good' --needed
@@ -31,38 +30,6 @@ BACK_PID=$!
 wait $BACK_PID
 clear
 
-
-
-cd $HOME
-
-#Picom
-echo -e "\e[1;31m${bold}Installing Picom${normal}\e[0m"
-git clone https://github.com/FT-Labs/picom
-cd picom
-meson --buildtype=release . build
-sudo ninja -C build install
-cd ..
-rm -rf picom
-clear
-
-
-
-#Zathura
-echo -e "\e[1;31m${bold}Installing zathura-pywal${normal}\e[0m"
-git clone https://github.com/mlscarlson/zathura-pywal/
-cd zathura-pywal
-sudo make install
-cd ..
-rm -rf zathura-pywal
-clear
-
-
-#NvChad installation
-echo -e "\e[1;31m${bold}Installing nvchad${normal}\e[0m"
-git clone https://github.com/NvChad/NvChad $CONFIG_DIR/nvim --depth 1
-ln -sf $HOME/.dotfiles_new/files/nvchad/custom $CONFIG_DIR/nvim/lua/
-clear
-
 #Spicetify
 echo -e "\e[1;31m${bold}Configuring spicetify${normal}\e[0m"
 sudo chmod a+wr /opt/spotify
@@ -73,10 +40,4 @@ spicetify config current_theme ziro
 spicetify apply
 clear
 
-#Shell
-echo -e "\e[1;31m${bold}Changing shell${normal}\e[0m"
-chsh -s $(which zsh)
-BACK_PID=$!
-wait $BACK_PID
-clear
 
