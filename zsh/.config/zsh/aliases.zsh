6d1d1e8ec6f930e5cac6b574cf77ce93cdb5c5eb
k-wachira <abkelvinwachira@gmail.com>
2023-04-03 22:41:59 +0300

    Move from ohmyzsh to zap plugin manager

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,17 +1,21 @@
 #!/bin/sh
 alias j='z'
 alias f='zi'
+alias p='z -'
+
+alias jp='j PERSONAL'
 
 alias vim="nvim"
 alias 1s="/home/init/.screenlayout/1screen.sh"
 alias 2s="/home/init/.screenlayout/2screens.sh"
 alias sd="/home/init/.config/scripts/shutdown2.sh"
 
-
+alias jn="j nvim && vim ."
 # confirm before overwriting something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
+alias md='mkdir -p'
 
 # get top process eating cpu ##
 alias cpu='ps auxf | sort -nr -k 3 | head -5'
--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,5 +1,6 @@
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
 export PATH="$PATH:$HOME/.rvm/gems/ruby-3.2.1/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
 
 eval "$(zoxide init zsh)"
